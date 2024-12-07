b8d451b429dcbbce17593455429aa38a8755b1d4
Felipe.Francca <dev.felipefrancca@gmail.com>
2024-12-07 16:11:23 -0300

    some changes to fix the deploy method for nextjs on gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, 
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/gamePalavraDoDia' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/gamePalavraDoDia' : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
