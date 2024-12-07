a2c02d64d48a168ddc8b0c14c4a0dde5062e1dec
Felipe.Francca <dev.felipefrancca@gmail.com>
2024-12-07 16:14:10 -0300

    up

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/gamePalavraDoDia' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/gamePalavraDoDia' : '',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
